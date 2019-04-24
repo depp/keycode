6b9bd410a7013183fd297713e01b4a3ec78e7f63
Dietrich Epp <depp@zdome.net>
2019-04-23 22:36:28 -0400

    Write new script for extracting keycodes
    The new script runs on Python 3. The output uses the standard CSV
    format, rather than using tabs, so it should be more interoperable with
    other systems. A header with column names is added to the output. Linux
    keycodes are taken from input-event-codes.h instead of trying to read
    XKB configuration files.

--- scripts/README.md
+++ scripts/README.md
@@ -5,3 +5,16 @@ These scripts are incomplete, they are a new version of the scripts in the paren
 These scripts generate the keycode tables. You do not need to run these scripts yourself, because the outputs are checked in to the repository.
 
 Running these scripts requires Python 3.
+
+## Extracting Platform Keycodes
+
+Various platforms define keycodes in header files in their SDK. The `extract.py` program extracts these definitions and puts them in CSV format. You don’t need to run this program yourself, the results are already available in the `data` directory. You can re-run this program if the SDK is updated with additional definitions (which does happen).
+
+The program should automatically find the header files on Linux and macOS, if they are installed.
+
+    python extract.py --platform=linux
+    python extract.py --platform=macos
+
+For Windows, you must specify the path to the `WinUser.h` header file in the Windows SDK.
+
+    python extract.py --platform=windows --input=path/to/WinUser.h
--- scripts/extract.py
+++ scripts/extract.py
@@ -1,16 +1,20 @@
+# Copyright 2019 Dietrich Epp.
+# This file is licensed under the terms of the MIT license. See LICENSE.txt
+# for details.
 """Extract keycode tables from various SDK headers."""
 import argparse
+import csv
 import os
 import re
 import sys
 
 
-def die(*msg):
-    print("Error:", *msg, file=sys.stderr)
-    raise SystemExit(1)
+class Error(Exception):
+    """An error that occurs during keycode extraction."""
 
 
 def linux_read_table(infile):
+    """Read the Linux keymap from linux/input-event-codes.h."""
     if infile is None:
         infile = "/usr/include/linux/input-event-codes.h"
     with open(infile, "rb") as fp:
@@ -21,23 +25,23 @@ def linux_read_table(infile):
 
 
 def macos_open_header(infile):
+    """Open the Carbon Events.h header file on macOS.
+
+    Searches various SDK installation locations.
+    """
     if infile is not None:
         return open(infile, "rb")
     # The headers may be in one of these places.
     bases = [
         "/",
-        (
-            "/Applications/Xcode.app/Contents/Developer/Platforms"
-            + "/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
-        ),
-        "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk",
+        ("/Applications/Xcode.app/Contents/Developer/Platforms" +
+         "/MacOSX.platform/Developer/SDKs/MacOSX.sdk"),
+        "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
     ]
     for base in bases:
         fpath = os.path.join(
-            base,
-            "System/Library/Frameworks/Carbon.framework"
-            + "/Frameworks/HIToolbox.framework/Headers/Events.h",
-        )
+            base, "System/Library/Frameworks/Carbon.framework" +
+            "/Frameworks/HIToolbox.framework/Headers/Events.h")
         try:
             return open(fpath, "rb")
         except FileNotFoundError:
@@ -54,6 +58,7 @@ def macos_read_table(infile):
 
 
 def windows_read_table(infile):
+    """Read the Windows keymap from WinUser.h."""
     if infile is None:
         die("Cannot find WinUser.h automatically, use --input.")
     with open(infile, "rb") as fp:
@@ -70,38 +75,65 @@ PLATFORMS = {
 
 
 def write_table(table, fp):
+    """Write a keycode table to a file in CSV format."""
+    writer = csv.writer(fp)
+    writer.writerow(["Keycode", "Name"])
     for row in table:
-        fp.write("{},{}\n".format(*row))
+        writer.writerow(row)
+
+
+def extract(*, infile, outfile, platform, quiet=False):
+    """Extract keycodes from a header file.
+
+    Arguments:
+      infile: Input file path, or None to automatically locate it
+      outfile: Output file path, or None to automatically place it
+      platform: Name of platform
+    """
+    read_table = PLATFORMS[platform]
+    try:
+        table = list(read_table(infile))
+    except FileNotFoundError as ex:
+        raise Error("Input file not found: {!r}".format(infile))
+    if not table:
+        raise Error(
+            "Could not find keycode definitions in input file {!r}".format(
+                infile))
+        die("Could not find keycode definitions in input file.")
+    if outfile is None:
+        repo = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        outfile = os.path.join(repo, "data",
+                               "{}_scancodes.csv".format(platform))
+    if not quiet:
+        print("Writing {}".format(outfile))
+    with open(outfile, "w") as fp:
+        write_table(table, fp)
 
 
 def main(argv):
     p = argparse.ArgumentParser(
-        description="Extract keycode tables from various SDK headers"
-    )
-    p.add_argument(
-        "--platform",
-        help="extract tables for PLATFORM",
-        metavar="PLATFORM",
-        choices=PLATFORMS,
-        required=True,
-    )
+        description="Extract keycode tables from various SDK headers")
+    p.add_argument("--platform",
+                   help="extract tables for PLATFORM",
+                   metavar="PLATFORM",
+                   choices=PLATFORMS,
+                   required=True)
     p.add_argument("--input", "-i", help="input header file")
     p.add_argument("--output", "-o", help="output CSV file")
+    p.add_argument("--quiet",
+                   "-q",
+                   help="print no informational messages",
+                   action="store_true")
     args = p.parse_args(argv)
 
-    read_table = PLATFORMS[args.platform]
     try:
-        table = list(read_table(args.input))
-    except FileNotFoundError as ex:
-        die(ex)
-    if not table:
-        die("Could not find keycode definitions in input file.")
-    output = args.output
-    if output is None:
-        repo = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        output = os.path.join(repo, "data", args.platform + "_scancodes.csv")
-    with open(output, "w") as fp:
-        write_table(table, fp)
+        extract(infile=args.input,
+                outfile=args.output,
+                platform=args.platform,
+                quiet=args.quiet)
+    except Error as ex:
+        print("Error:", ex, file=sys.stderr)
+        raise SystemExit(1)
 
 
 if __name__ == "__main__":
